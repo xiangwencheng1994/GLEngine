973a7ba99e907460e3eaf79b1af5812f8c4329c0
xiangwencheng <xiangwencheng@outlook.com>
2019-05-07 01:13:30 +0800

    update temp

--- sge/include/core/sgeImage.h
+++ sge/include/core/sgeImage.h
@@ -1,12 +1,72 @@
 #pragma once
 
+#include <core/sgeDefs.h>
+
 namespace sge
 {
-    
+    /**
+     * Enum PixelFormat
+     * Possible pixel formats
+     */
+    enum PixelFormat
+    {
+        //! 32-bit texture: BGRA8888
+        PF_BGRA8888,
+        //! 32-bit texture: RGBA8888
+        PF_RGBA8888,
+        //! 24-bit texture: RGBA888
+        PF_RGB888,
+        //! 16-bit texture without Alpha channel
+        PF_RGB565,
+        //! 8-bit textures used as masks
+        PF_A8,
+        //! 8-bit intensity texture
+        PF_I8,
+        //! 16-bit textures used as masks
+        PF_AI88,
+        //! 16-bit textures: RGBA4444
+        PF_RGBA4444,
+        //! 16-bit textures: RGB5A1
+        PF_RGB5A1,
+        //! 4-bit PVRTC-compressed texture: PVRTC4
+        PF_PVRTC4,
+        //! 4-bit PVRTC-compressed texture: PVRTC4 (has alpha channel)
+        PF_PVRTC4A,
+        //! 2-bit PVRTC-compressed texture: PVRTC2
+        PF_PVRTC2,
+        //! 2-bit PVRTC-compressed texture: PVRTC2 (has alpha channel)
+        PF_PVRTC2A,
+        //! ETC-compressed texture: ETC
+        PF_ETC,
+        //! S3TC-compressed texture: S3TC_Dxt1
+        PF_S3TC_DXT1,
+        //! S3TC-compressed texture: S3TC_Dxt3
+        PF_S3TC_DXT3,
+        //! S3TC-compressed texture: S3TC_Dxt5
+        PF_S3TC_DXT5,
+        //! ATITC-compressed texture: ATC_RGB
+        PF_ATC_RGB,
+        //! ATITC-compressed texture: ATC_EXPLICIT_ALPHA
+        PF_ATC_EXPLICIT_ALPHA,
+        //! ATITC-compressed texture: ATC_INTERPOLATED_ALPHA
+        PF_ATC_INTERPOLATED_ALPHA,
+        //! ERROR Format
+        PF_NONE = -1
+    };
+
+    /**
+     * Class Image resource
+     */
     class Image
     {
     public:
-        
+        Image(const char* imgFile);
+        Image(const byte* data, size_t len);
+
+    private:
+        PixelFormat     _pixelFormat;
+
+        DISABLE_COPY(Image)
     };
 
 }
--- sge/include/graphics/sgeGraphicsDevice.h
+++ sge/include/graphics/sgeGraphicsDevice.h
@@ -2,6 +2,7 @@
 
 #include <core/sgeDefs.h>
 #include <core/sgeMath.h>
+#include <core/sgeImage.h>
 
 namespace sge
 {
@@ -60,60 +61,7 @@ namespace sge
     constexpr NativeShaderProgramId kInvalidProgramId = NULL;
 
 
-    /**
-     * Enum PixelFormat
-     * Possible texture pixel formats
-     * @seealso https://github.com/cocos2d/cocos2d-x/blob/v3/cocos/renderer/CCTexture2D.h
-     */
-    enum PixelFormat
-    {
-        //! auto detect the type
-        AUTO,
-        //! 32-bit texture: BGRA8888
-        BGRA8888,
-        //! 32-bit texture: RGBA8888
-        RGBA8888,
-        //! 24-bit texture: RGBA888
-        RGB888,
-        //! 16-bit texture without Alpha channel
-        RGB565,
-        //! 8-bit textures used as masks
-        A8,
-        //! 8-bit intensity texture
-        I8,
-        //! 16-bit textures used as masks
-        AI88,
-        //! 16-bit textures: RGBA4444
-        RGBA4444,
-        //! 16-bit textures: RGB5A1
-        RGB5A1,
-        //! 4-bit PVRTC-compressed texture: PVRTC4
-        PVRTC4,
-        //! 4-bit PVRTC-compressed texture: PVRTC4 (has alpha channel)
-        PVRTC4A,
-        //! 2-bit PVRTC-compressed texture: PVRTC2
-        PVRTC2,
-        //! 2-bit PVRTC-compressed texture: PVRTC2 (has alpha channel)
-        PVRTC2A,
-        //! ETC-compressed texture: ETC
-        ETC,
-        //! S3TC-compressed texture: S3TC_Dxt1
-        S3TC_DXT1,
-        //! S3TC-compressed texture: S3TC_Dxt3
-        S3TC_DXT3,
-        //! S3TC-compressed texture: S3TC_Dxt5
-        S3TC_DXT5,
-        //! ATITC-compressed texture: ATC_RGB
-        ATC_RGB,
-        //! ATITC-compressed texture: ATC_EXPLICIT_ALPHA
-        ATC_EXPLICIT_ALPHA,
-        //! ATITC-compressed texture: ATC_INTERPOLATED_ALPHA
-        ATC_INTERPOLATED_ALPHA,
-        //! Default texture format: AUTO
-        DEFAULT = AUTO,
 
-        NONE = -1
-    };
 
     /**
      * The wrap parameter for texture
